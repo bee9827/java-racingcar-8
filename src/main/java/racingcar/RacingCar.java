3cbdc268435c9b650be3cb1ce1d3dfd01d8d5b8d
bee9827 <ehfrhfo9494@naver.com>
2025-10-22 15:20:09 +0900

    feat: 무작위 값을 생성한다
    0~9 사이의 값을 생성한다.

--- src/main/java/racingcar/RacingCar.java
+++ src/main/java/racingcar/RacingCar.java
@@ -3,10 +3,10 @@ package racingcar;
 import java.util.regex.Pattern;
 
 public class RacingCar {
-    public static final int DEFAULT_LOCATION = 0;
     public static final int MIN_VALUE = 0;
     public static final int MAX_VALUE = 9;
 
+    private static final int DEFAULT_LOCATION = 0;
     private static final int MOVE_THRESHOLD = 4;
     private static final Pattern NAME_REGEX = Pattern.compile("^[A-Za-z0-9가-힣-]{1,5}$");
 
---
+++ src/main/java/racingcar/RandomValueGeneratorImpl.java
@@ -0,0 +1,10 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RandomValueGeneratorImpl implements RandomValueGenerator {
+    @Override
+    public int generate() {
+        return Randoms.pickNumberInRange(RacingCar.MIN_VALUE, RacingCar.MAX_VALUE);
+    }
+}
