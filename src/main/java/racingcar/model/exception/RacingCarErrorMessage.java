ba0c188fa42ecb6a8716d28609c8e6191fa165e3
bee9827 <ehfrhfo9494@naver.com>
2025-10-24 20:34:34 +0900

    feat: 예외 문구 포메팅 예외처리

--- src/main/java/racingcar/model/exception/RacingCarErrorMessage.java
+++ src/main/java/racingcar/model/exception/RacingCarErrorMessage.java
@@ -1,11 +1,13 @@
 package racingcar.model.exception;
 
+import java.util.IllegalFormatException;
+
 public enum RacingCarErrorMessage {
     INVALID_POSITION("유효하지 않은 위치입니다: %d"),
     INVALID_NAME("유효하지 않은 이름입니다: %s"),
     DUPLICATE_CAR_NAME("중복된 자동차 이름입니다: %s"),
     INVALID_STATE_RANDOM_VALUE("randomValue: %d ~ %d 사이의 값만 허용됩니다."),
-    ;
+    INVALID_STATE_ARGS("형식에 맞춰 인자를 입력해 주세요 [예외: %s]");
 
     private final String template;
 
@@ -14,6 +16,11 @@ public enum RacingCarErrorMessage {
     }
 
     public String format(Object... args) {
-        return String.format(template, args);
+        try {
+            return String.format(template, args);
+        } catch (IllegalFormatException e) {
+            String message = INVALID_STATE_ARGS.format(this);
+            throw new RacingCarStateException(message);
+        }
     }
 }
