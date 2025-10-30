34b226b61b47f479d675ed1871bbd0d27498997a
bee9827 <ehfrhfo9494@naver.com>
2025-10-30 23:31:42 +0900

    refactor: StateException 메시지 개행

--- src/main/java/racingcar/model/exception/RacingCarErrorMessage.java
+++ src/main/java/racingcar/model/exception/RacingCarErrorMessage.java
@@ -7,6 +7,7 @@ public enum RacingCarErrorMessage {
     INVALID_NAME("유효하지 않은 이름입니다: %s"),
     DUPLICATE_CAR_NAME("중복된 자동차 이름입니다: %s"),
     INVALID_ATTEMPT_COUNT("유효하지 않은 시도 횟수입니다. %s"),
+
     INVALID_STATE_ATTEMPT_COUNT("가능한 시도 횟수는 %s 입니다. 현재: %s"),
     INVALID_STATE_RANDOM_VALUE("randomValue: %d ~ %d 사이의 값만 허용됩니다."),
     INVALID_STATE_ARGS("형식에 맞춰 인자를 입력해 주세요 [예외: %s]");
